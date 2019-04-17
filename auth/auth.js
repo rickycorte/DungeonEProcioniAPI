736968bd9d14fcaad36638d6668f0b5ea0fb46da
Riccardo <rickycortef@gmail.com>
2019-04-17 23:05:36 +0200

    Added login

--- auth/auth.js
+++ auth/auth.js
@@ -7,9 +7,43 @@ const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
-const User = require("@models/user.js")
+const User = require("@models/user.js");
 
+const checkMw = require("./tokenCheckMiddleware.js");
+
+
+/**
+ * Make a jwt token based on mongodb id
+ * @param {*} userID 
+ */
+function makeJWTToken(userID) {
+    return jwt.sign(
+        { id: userID },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_TOKEN_LIFETIME_SECONDS }
+    );
+}
+
+
+/* ======================================================================================== */
+// ROUTES
+
+
+/**
+ * Register a new User
+ */
 router.post('/register', function (req, res) {
+
+    //TODO:sistemare per la production, ora la tengo solo in debug
+
+    if (process.env.NODE_ENV === "production") {
+        res.status(500).send({
+            result: "error",
+            token: "Our raccoon engineers are working hard to let you join the beta as soon as possibile!"
+        });
+    }
+
+    //TODO: controllare che non esista gia un utente con quella mail
 
     try {
         var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -20,37 +54,112 @@ router.post('/register', function (req, res) {
             password: hashedPassword
         });
 
-        usr.save( (err, user) => {
-                if (err) 
-                return res.status(500).send({result: "error", message: "Oh no! A raccoon broke our database!"});
-
-                // create a token
-                var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_TOKEN_LIFETIME_SECONDS
+        usr.save((err, user) => {
+            if (err)
+                return res.status(500).send({
+                    result: "error",
+                    message: "Oh no! A raccoon broke our database!"
                 });
 
-                console.log("Registerd new user!");
-                res.status(200).send({ result: "ok", token: token });
+            // create a token
+            var token = makeJWTToken(user._id);
+
+            console.log("Registered a new user!");
+            res.status(200).send({
+                result: "ok",
+                token: token
             });
+
+        });
     }
     catch (err) {
-        return res.status(500).send({result: "error", message: "Please check your request data!" } );
+        return res.status(400).send({
+            result: "error",
+            message: "Please check your request data!"
+        });
     }
 
 });
 
 
+/**
+ * Check if current token is valid
+ */
+router.get('/check', checkMw, function (req, res) {
 
-router.get('/check', function (req, res) {
+        res.status(200).send({
+            result: "ok",
+            message: "Authenticated"
+        });
+});
 
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ result: "error", auth: false, message: 'No token provided.' });
 
-    jwt.verify(token, config.secret, function (err, decoded) {
-        if (err) return res.status(500).send({ result: "error", auth: false, message: 'Failed to authenticate token.' });
+/**
+ * Get user profile
+ */
+router.get('/profile', checkMw, function (req, res) {
 
-        res.status(200).send({result: "ok", message: "Authenticated"});
+
+    User.findById(req.userId, { password: 0, __v: 0 }, 
+        (err, user) => {
+        if (err) {
+            return res.status(500).send({status: "error", message:"There was a problem finding the user."});
+        }
+        if (!user) return res.status(404).send({status:"error", message: "No user found."});
+    
+        res.status(200).send(user);
     });
+
+});
+
+
+/**
+ * Log the user in
+ */
+router.post('/login', function (req, res) {
+
+    try {
+
+        User.findOne({
+            email: req.body.email
+        },
+            (err, user) => {
+                if (err)
+                    return res.status(500).send({
+                        status: "error",
+                        auth: false,
+                        message: "A raccoon broke our database, please retray later!"
+                    });
+
+                if (!user) {
+                    return res.status(404).send({
+                        status: "error",
+                        auth: false,
+                        message: "Our raccoouns can't find your account, sorry."
+                    });
+                }
+
+                var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+                if (!passwordIsValid) {
+                    return res.status(404).send({
+                        status: "error",
+                        auth: false,
+                        message: "Not a valid account"
+                    });
+                }
+
+                var token = makeJWTToken(user._id);
+
+                res.status(200).send({ status: "ok", auth: true, token: token });
+            });
+    }
+    catch (err) {
+        return res.status(400).send({
+            result: "error",
+            message: "Please check your request data!"
+        });
+    }
+
 });
 
 module.exports = router;
